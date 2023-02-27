c177210eb8b9324d1bfddeffde759b57c12b3142
mtsmendes <123379587+mtsmendes@users.noreply.github.com>
2023-02-27 12:57:39 -0300

    Add files via upload

---
+++ scripts/controle.js
@@ -0,0 +1,151 @@
+var controlador = true
+var conteudoBtns = ["%"  ,  "CE" ,   "C"   , "backspace", 
+                    "¹/x",  "x²" ,   "²√"   , "÷",  
+                    "7"  ,  "8"  ,   "9"   , "X", 
+                    "4"  ,  "5 " ,   "6"   , "-", 
+                    "1"  ,  "2"  ,   "3"   , "+", 
+                    "+/-",  "0"  ,   ","   , "="];
+
+var funcoesBtns = ["operacoesEspeciais('P')", "limparDados(false)", "limparDados()", "apagarCaracter()",
+                    "operacoesEspeciais('D')", "operacoesEspeciais('E')", "operacoesEspeciais('R')", "calculoAritmetico('*')",
+                    "addDisplay(7)", "addDisplay(8)", "addDisplay(9)", "calculoAritmetico('*')", 
+                    "addDisplay(4)", "addDisplay(5)", "addDisplay(6)", "calculoAritmetico('-')",
+                    "addDisplay(1)", "addDisplay(2)", "addDisplay(3)", "calculoAritmetico('+')", 
+                    "inverterSinal()", "addDisplay(0)","addPonto()", "calculoAritmetico('=')"]; 
+
+
+criarBotoesCalc();
+
+
+//cria os botoes do teclado
+function criarBotoesCalc() {
+    criarLinhasBtns(0, 3);
+    criarLinhasBtns(4, 7);
+    criarLinhasBtns(8, 11);
+    criarLinhasBtns(12, 15);
+    criarLinhasBtns(16, 19);
+    criarLinhasBtns(20, 23);
+
+}
+
+function criarLinhasBtns(indiceInicio, indiceFim) { // botao-numeros, "botao-operacoes"
+    let container = document.querySelector(".container-botoes-calc");
+    let div = criarTag("div");
+    let btn;
+    
+    addAtributo(div, "class", "container-linha-botoes");
+
+    for (let i = indiceInicio; i <=indiceFim; i ++) {
+        btn = criarTag("input");
+        addAtributo(btn, "type", "button");
+        addAtributo(btn, "value", conteudoBtns[i]);
+        addAtributo(btn, "onclick", funcoesBtns[i]);
+
+
+        if ((indiceInicio >= 0 && indiceFim <= 7) || i == 11 || i ==15 ||  i == 19) {
+            addAtributo(btn, "class", "botao-operacoes config-btn");
+            if (i == 3) {
+                addAtributo(btn, "class", "botao-operacoes config-btn material-symbols-outlined");
+            }
+        }
+        else if (i == 23) {
+            addAtributo(btn, "class", "botao-igual config-btn");
+        }
+        else {
+            addAtributo(btn, "class", "botao-numeros config-btn");
+        }
+
+        div.appendChild(btn);
+    }
+
+    container.appendChild(div);
+}
+
+//auxilixar para criar as tag
+function criarTag(tag) {
+    return document.createElement(tag);
+}
+
+//auxiliar ao colocar atributos a um elemento
+function addAtributo(elemento, atributo, valor) {
+    elemento.setAttribute(atributo, valor);
+}
+
+/**== CONTROLADORES ==*/
+
+//controla a abertura de memoria e histórico
+function ativarAuxiliares(parametro = false, caracter = 'H', diminuirTela = false) {
+    let principal   = document.querySelector(".principal");
+    let calculadora = document.querySelector(".container-calculadora");
+    let auxiliares  = document.querySelector(".container-auxiliares");
+    let btnMemoria  = document.querySelector("#btn-mostrar-memoria");
+
+
+    if (parametro){
+        controlador = true;
+    }
+
+    if(diminuirTela) {
+        controlador = false;
+    }
+
+
+    if (controlador == true) {
+
+        principal.style.width    = "60vw";
+        calculadora.style.width  = "50%";
+        auxiliares.style.display = "flex";
+        btnMemoria.style.display = "none";
+
+        if (caracter == 'H') {
+
+            console.log("entrou");
+            document.getElementById("btn-historico").focus();
+            ativarHistorico(true);
+        }
+       if (caracter == 'M') {
+            
+            document.getElementById("btn-memoria").focus();
+            ativarHistorico(false);
+        }
+    }
+    else {
+
+        principal.style.width    = "30vw";  
+        calculadora.style.width  = "100%";
+        auxiliares.style.display = "none";
+        btnMemoria.style.display = "inline";
+
+    }
+
+     //vai receber sempre o valor ao contrário que se encontra
+     controlador = !controlador;
+
+}
+
+function ativarHistorico(parametro) {
+    let historico = document.querySelector(".container-historico");
+    let memoria = document.querySelector(".container-memoria");
+
+    if (parametro) {
+
+        historico.style.display = "flex";
+        memoria.style.display   = "none";
+        carregarHistorico();
+    }
+    else {
+        
+        historico.style.display = "none";
+        memoria.style.display   = "flex";
+        carregarMemoria();
+    }
+}
+
+
+//controlar o tamanho da tela para mudar o display
+window.addEventListener('resize', function () {
+    var largura = window.innerWidth;
+
+    if (largura < 650) 
+    ativarAuxiliares(false, null, true);
+});
---
+++ scripts/script.js
@@ -0,0 +1,410 @@
+
+//variáveis globais
+var pilha = [], sinal = [], ultimosValores = [], memoria = [], historico = [];
+var limparDisplay = true;
+var ultimoSinal;
+
+
+//displays
+let display = document.querySelector(".display");
+let subDisplay = document.querySelector(".sub-display");
+display.value = 0;
+
+
+//funcao principal
+function calculoAritmetico(parametro) {
+
+    if (sinal.length == 0 && parametro == '='){
+        display.value = "0";
+        return
+    } 
+    else {
+        sinal.push(parametro);
+    }
+
+    if (sinal[0] == '=' && sinal[1] == sinal[0] && pilha.length > 0) {
+        repetirUltimaOperacao();
+    }
+    else if (sinal[0] == '=' && sinal[1] != sinal[0]) {
+        let valor = display.value;
+        let sinal = parametro;
+        limparDados();
+        display.value = valor;
+        calculoAritmetico(sinal);
+    }
+    else {
+
+        pilha.push(Number(display.value));
+        calcular();
+
+        escreverSubDisplay(false, sinal[0]);
+        display.value = pilha[0];
+
+        limparDisplay = true;
+    }
+}
+
+//efetua a operação
+function calcular() {
+    if (pilha.length == 2) {
+
+        let valor2 = pilha.pop();
+        let valor1 = pilha.pop();
+        let valor;
+
+        switch (sinal[0]) {
+            case '+':
+                valor = valor1 + valor2;
+                pilha.push(valor);
+                break;
+
+            case '-':
+                valor = valor1 - valor2;
+                pilha.push(valor);
+                break;
+
+            case '*':
+                valor = valor1 * valor2;
+                pilha.push(valor);
+                break;
+
+            case '/':
+                valor = valor1 / valor2;
+                pilha.push(valor);
+                break;
+
+            case '=':
+                // display.value = pilha[0]; 
+                display.value = "inplementar sinal =";
+                break;
+
+            default:
+                window.alert("Ainda não implementado");
+                break;
+        }
+
+
+
+        ultimoSinal = sinal[0];
+        sinal[0] = sinal.pop();
+
+        ultimosValores[0] = valor1;
+        ultimosValores[1] = valor2;
+        ultimosValores[2] = valor;
+        addHistorico();
+    }
+}
+
+
+/* === Sub operações ===*/
+
+//efetua as operações especiais
+function operacoesEspeciais(parametro) {
+    let valor = Number(display.value);
+
+    switch (parametro) {
+
+        case 'P':
+            valor = porcentagem(valor);
+            break;
+
+        case 'E':
+            valor = valor ** 2;
+            break;
+
+        case 'R':
+            valor = Math.sqrt(valor);
+            break;
+
+        case 'D':
+            valor = 1 / valor;
+            break;
+
+        default:
+            break;
+    }
+
+    display.value = valor;
+}
+
+//calcula a porcentagem
+function porcentagem(parametro) {
+    let aux;
+
+    if (pilha.length == 0) {
+        aux = 0;
+    }
+    else {
+        aux = pilha[0];
+    }
+
+    return (aux * parametro) / 100;
+}
+
+//adiciona ' . ' a tela
+function addPonto() {
+
+    if (!display.value.includes(".")) {
+        display.value += ".";
+    }
+}
+
+//inverter sinal
+function inverterSinal() {
+    let valor = display.value;
+
+    if (valor != "0") {
+        if (valor.charAt(0) == '-') {
+            display.value = (valor.replace("-", ""));
+        }
+        else {
+            display.value = "-" + valor;
+        }
+    }
+}
+
+//somar ou diminuir memoria
+function maisOuMenosMemoria(parametro) {
+    
+    if (memoria.length > 0) {
+        let aux = memoria.length - 1;
+        if(parametro == '+') {
+            memoria [aux] += Number(display.value);
+        }
+        else {
+            memoria [aux] -= Number(display.value);
+        }
+
+        carregarMemoria();
+    }
+}
+
+//apaga ultimo caracter
+function apagarCaracter() {
+    let valor = display.value.slice(0, -1);
+
+    if (valor.length > 0) {
+        display.value = valor;
+    }
+    else {
+        display.value = 0;
+    }
+}
+
+/* === Funções auxiliares ===*/
+
+
+// adiciona os numeros no display
+function addDisplay(valor) {
+
+    if (limparDisplay) {
+        limparDisplay = false;
+        display.value = null;
+    }
+
+    display.value += valor;
+}
+
+//limpa todos os parametros
+function limparDados(limparTudo = true) {
+    if (limparTudo) {
+        limparVetor(pilha);
+        limparVetor(sinal);
+        limparVetor(ultimosValores);
+        limparDisplay = true;
+        escreverSubDisplay(true);
+        ultimoSinal = undefined;
+    }
+
+    display.value = 0;
+    limparDisplay = true;
+}
+
+//escreve no subDisplay
+function escreverSubDisplay(limpar = false, parametro) {
+
+    if (limpar) {
+        subDisplay.innerHTML = "";
+    }
+    else {
+
+        switch (parametro) {
+            case '*':
+                subDisplay.innerHTML = `${pilha[0]} x`;
+                break;
+            case '=':
+                let aux;
+
+                if (ultimoSinal == '*') { 
+                    aux = 'x' 
+                } 
+                else { 
+                    aux = ultimoSinal 
+                }
+
+                subDisplay.innerHTML = `${ultimosValores[0]} ${aux}  ${ultimosValores[1]} ${parametro} `;
+                break;
+
+            default:
+                subDisplay.innerHTML = `${pilha[0]} ${sinal[0]}`;
+                break;
+        }
+    }
+
+}
+
+//calculo quando der == + == novamente
+function repetirUltimaOperacao() {
+    let valor = pilha.pop();
+    let auxSinal;
+
+    switch (ultimoSinal) {
+        case '+':
+            valor = valor + ultimosValores[1];
+            break;
+
+        case '-':
+            valor = valor - ultimosValores[1];
+            break;
+
+        case '*':
+            valor = valor * ultimosValores[1];
+            break;
+
+        case '/':
+            valor = valor / ultimosValores[1];
+            break;
+
+        default:
+            console.log("erro no switch do == e == ");
+            break;
+    }
+
+    pilha.push(valor);
+
+    if (ultimoSinal == '*') {
+        auxSinal = 'x'
+    }
+    else {
+        auxSinal = ultimoSinal;
+    }
+
+   subDisplay.innerHTML = `${pilha[0]} ${auxSinal} ${ultimosValores[1]} = `;
+    display.value = pilha[0];
+
+    
+
+
+
+    addHistorico();
+}
+
+//para limpar os vetores
+function limparVetor(vetor) {
+    for (let i = vetor.length - 1; i >= 0; i--) {
+        vetor.pop();
+    }
+}
+
+
+/* FUNCÇÕES DA MEMORIA  */
+
+
+//adiciona no historico
+function addHistorico(){
+    let array     = [2];
+
+    //criando uma array by dimencional
+    array[0] = `${ultimosValores[0]} ${ultimoSinal} ${ultimosValores[1]} =`;
+    array[1] = ultimosValores[2];
+
+    historico.push(array);
+
+    carregarHistorico();
+}
+
+//carrega o historico
+function carregarHistorico() {
+    let history = document.querySelector(".armazena-historico");
+    let conteudo, tag;
+
+    history.innerHTML = "";
+
+    if (historico.length == 0) {
+        history.innerHTML = "Não constam históricos."
+    }
+    else {
+        for (let i = historico.length -1; i >= 0; i--) {
+
+            tag = document.createElement("p");
+            conteudo = historico[i][0] + historico[i][1];
+            tag.classList.add("conteudo-memoria");
+            tag.setAttribute("onclick", "jogarHistoriaDisplay(" + i + ")");
+            tag.appendChild(document.createTextNode(conteudo));
+            history.appendChild(tag);
+        }
+    }
+}
+
+//joga o historico no display
+function jogarHistoriaDisplay(parametro) {
+    subDisplay.innerHTML = historico[parametro][0];
+    display.value = historico[parametro][1];
+}
+
+//limpar histórico
+function limparHistorico(){
+   limparVetor(historico);
+   carregarHistorico();
+}
+
+
+/* FUNCÇÕES DA MEMORIA  */
+
+//inserir na memoria
+function addMemoria(){
+    memoria.push(Number(display.value));
+    carregarMemoria();
+    limparDisplay = true;
+}
+   
+//limpa memoria
+function limpaMemoria() {
+    limparVetor(memoria);
+    carregarMemoria();
+}
+
+//limpa e carrega  a memoria novamente
+function carregarMemoria() {
+    let memory = document.querySelector(".armazena-memoria");
+    let conteudo, tag;
+
+    //limpando a tag toda
+    memory.innerHTML = "";
+
+    if (memoria.length == 0) {
+        memory.innerHTML = "Não constam valores na memória."
+    }
+    else {
+        for (let i = memoria.length -1; i >= 0; i--) {
+            tag = document.createElement("p");
+            conteudo = memoria[i];
+            tag.classList.add("conteudo-memoria");
+            tag.setAttribute("onclick", "jogarMemoriaDisplay(" + i + ")");
+            tag.appendChild(document.createTextNode(conteudo));
+            memory.appendChild(tag);
+        }
+    }
+}
+
+//pega o numero da memoria
+function pegarMemoria() {
+    if (memoria.length > 0){
+        display.value = memoria[memoria.length - 1];
+    }
+}
+
+//joga memorria no display
+function jogarMemoriaDisplay(parametro) {
+    display.value = memoria[parseInt(parametro)];
+}
